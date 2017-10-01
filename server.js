9dda8e96d06b2dce815ea876c8b80c2dfd70dd02
NitinBuzz <reachnitinvenkat@gmail.com>
2017-10-02 04:15:32 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('CapIt', string => {
+  return string.toUpperCase();
+});
+
+app.use((req, res, next) => {
+  res.render('down.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is up on port 3000`);
+});
